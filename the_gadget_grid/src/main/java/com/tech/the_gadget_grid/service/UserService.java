2c8738c39bd2eaeffcf820018f7a8a10a5a06cb0
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-16 17:01:47 +0530

    implement UserService for user registration and fetching

---
+++ the_gadget_grid/src/main/java/com/tech/the_gadget_grid/service/UserService.java
@@ -0,0 +1,23 @@
+package com.tech.the_gadget_grid.service;
+
+import com.tech.the_gadget_grid.entity.User;
+import com.tech.the_gadget_grid.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
