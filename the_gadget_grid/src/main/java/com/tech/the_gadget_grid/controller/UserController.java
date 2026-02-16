57c2a4a7601e035ddfd4fa9d0c23779a437bccf7
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-16 17:00:13 +0530

    implement UserController for user registration and retrieval

---
+++ the_gadget_grid/src/main/java/com/tech/the_gadget_grid/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.tech.the_gadget_grid.controller;
+
+import com.tech.the_gadget_grid.entity.User;
+import com.tech.the_gadget_grid.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/all")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
