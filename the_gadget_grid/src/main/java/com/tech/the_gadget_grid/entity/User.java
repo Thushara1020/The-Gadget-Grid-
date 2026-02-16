52dcac0d773990f34baebfd56524e264d2e6ac6f
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-16 17:01:04 +0530

    create User entity with JPA mapping and Lombok

---
+++ the_gadget_grid/src/main/java/com/tech/the_gadget_grid/entity/User.java
@@ -0,0 +1,32 @@
+package com.tech.the_gadget_grid.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String contactNumber;
+}
