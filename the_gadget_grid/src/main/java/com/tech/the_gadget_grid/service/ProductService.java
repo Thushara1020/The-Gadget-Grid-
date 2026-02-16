1111f23585db24335f13f774495547fa1d76bb35
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-16 17:52:28 +0530

    implement ProductService with full CRUD operations

---
+++ the_gadget_grid/src/main/java/com/tech/the_gadget_grid/service/ProductService.java
@@ -0,0 +1,31 @@
+package com.tech.the_gadget_grid.service;
+
+import com.tech.the_gadget_grid.entity.Product;
+import com.tech.the_gadget_grid.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
