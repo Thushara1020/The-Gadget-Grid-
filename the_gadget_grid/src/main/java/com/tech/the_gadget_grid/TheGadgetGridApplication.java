e79bc7b8d574e13de88cce22eb5956035d9f7c50
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-16 16:34:40 +0530

    Initial commit

---
+++ the_gadget_grid/src/main/java/com/tech/the_gadget_grid/TheGadgetGridApplication.java
@@ -0,0 +1,13 @@
+package com.tech.the_gadget_grid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGadgetGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGadgetGridApplication.class, args);
+	}
+
+}
---
+++ the_gadget_grid/src/test/java/com/tech/the_gadget_grid/TheGadgetGridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.the_gadget_grid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGadgetGridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
