3664e64911e3fb57e288e381f9283fbc0acc223d
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-03-01 21:01:28 +0530

    feat: Add OrderItemRepository to handle OrderItem database operations

---
+++ the_gadget_grid/src/main/java/com/tech/the_gadget_grid/repository/OrderItemRepository.java
@@ -0,0 +1,12 @@
+package com.tech.the_gadget_grid.repository;
+
+import com.tech.the_gadget_grid.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findByOrder_OrderId(Long orderId);
+}
